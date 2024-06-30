2a05253905573c6b93ec2db72bd1d01eda757a63
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-06-30 22:31:07 +0200

    Integrated reusable code blocks

--- src/module.py
+++ src/module.py
@@ -1,3 +1,9 @@
 # Auto-generated file
 logging.debug('Starting process...')
 print('User logged in: user44')
+from eth_typing import (
+    AnyAddress,
+    ChecksumAddress,
+    HexStr,
+    Primitives,
+)
