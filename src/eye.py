7d2e8f597e5c9289d9528dc2354bc037396c0e88
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-07-08 22:31:07 +0200

    Inserted code blocks from input source

--- src/eye.py
+++ src/eye.py
@@ -1,1 +1,12 @@
 # Auto-generated file
+def apply_error_formatters(
+    error_formatters: Callable[..., Any],
+    response: RPCResponse,
+) -> RPCResponse:
+    if error_formatters:
+        formatted_resp = pipe(response, error_formatters)
+        return formatted_resp
+    else:
+        return response
+
+
