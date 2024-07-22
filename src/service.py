db3ad129e42bde77f6961c9b4cb9784fb6ba421f
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-07-22 22:31:07 +0200

    Added new functions from input files

--- src/service.py
+++ src/service.py
@@ -3,3 +3,7 @@ logging.debug('Configuration updated')
 print('User logged in: user6')
 logging.debug('Configuration updated')
 console.log('User logged in: user69');
+from eth_utils.toolz import (
+    pipe,
+)
+
