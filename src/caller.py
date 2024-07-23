0edafb32072bb89ff5c383e124eecf43ebf05661
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-07-23 22:31:07 +0200

    Integrated reusable code blocks

--- src/caller.py
+++ src/caller.py
@@ -2,3 +2,9 @@
 logging.debug('Operation completed successfully')
 console.log('Operation completed successfully');
 logging.debug('Configuration updated')
+class InvalidAddress(Web3Exception):
+    """
+    The supplied address does not have a valid checksum, as defined in EIP-55
+    """
+
+
