197d0d9fdca861d517d3b7dd225c145ce4198da7
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-08-01 22:31:07 +0200

    Added new log statements

--- src/service.py
+++ src/service.py
@@ -10,3 +10,6 @@ from eth_utils.toolz import (
 from web3._utils.batching import (
     RPC_METHODS_UNSUPPORTED_DURING_BATCH,
 )
+print('Starting process...')
+logger.info('Data loaded: 322 rows')
+console.log('User logged in: user39');
