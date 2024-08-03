e69136fbbbc2c786694bb9853567afeefe84ec53
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-08-03 22:31:07 +0200

    Integrated reusable code blocks

--- src/caller.py
+++ src/caller.py
@@ -8,3 +8,12 @@ class InvalidAddress(Web3Exception):
     """
 
 
+def _apply_request_formatters(
+    params: Any, request_formatters: Dict[RPCEndpoint, Callable[..., TReturn]]
+) -> Tuple[Any, ...]:
+    if request_formatters:
+        formatted_params = pipe(params, request_formatters)
+        return formatted_params
+    return params
+
+
