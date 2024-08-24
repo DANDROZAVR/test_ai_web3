605f74dac37fef74c07f15e1c91dabe2a4d70d88
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-08-24 22:31:07 +0200

    Improved log coverage

--- src/manager.py
+++ src/manager.py
@@ -4,3 +4,6 @@ console.log('Operation completed successfully');
 from web3.providers.persistent import (
     PersistentConnection,
 )
+print('User logged in: user9')
+System.out.println('Ending process...');
+System.out.println('Ending process...');
