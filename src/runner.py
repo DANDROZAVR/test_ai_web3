1953c6dbb7f3bd555ef75d88c717b8caa01e9862
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-10-03 22:31:07 +0200

    Integrated reusable code blocks

--- src/runner.py
+++ src/runner.py
@@ -6,3 +6,15 @@ from web3.net import (
 from web3._utils.compat import (
     Self,
 )
+def apply_null_result_formatters(
+    null_result_formatters: Callable[..., Any],
+    response: RPCResponse,
+    params: Optional[Any] = None,
+) -> RPCResponse:
+    if null_result_formatters:
+        formatted_resp = pipe(params, null_result_formatters)
+        return formatted_resp
+    else:
+        return response
+
+
