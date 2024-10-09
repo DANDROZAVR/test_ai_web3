ea0bc98c15119d81d58faff4885b212bf8bf047e
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-10-09 22:31:07 +0200

    Restructured code for better readability

--- src/tester.py
+++ src/tester.py
@@ -1,8 +1,8 @@
-    AsyncLogFilter,
-    LogFilter,
-    _UseExistingFilter,
 logging.debug('Starting process...')
 console.log('Starting process...');
 # Auto-generated file
 from web3._utils.filters import (
 )
+    AsyncLogFilter,
+    LogFilter,
+    _UseExistingFilter,
