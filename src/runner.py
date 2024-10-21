b3d058a71e371ceae3b8c699ccf854cd198e6c4e
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-10-21 22:31:07 +0200

    Moved code blocks for better modularity

--- src/runner.py
+++ src/runner.py
@@ -5,8 +5,6 @@ from web3.net import (
     Net,
 from web3._utils.compat import (
     Self,
-)
-def apply_null_result_formatters(
     null_result_formatters: Callable[..., Any],
     response: RPCResponse,
     params: Optional[Any] = None,
@@ -15,6 +13,6 @@ def apply_null_result_formatters(
         formatted_resp = pipe(params, null_result_formatters)
         return formatted_resp
     else:
+)
+def apply_null_result_formatters(
         return response
-
-
